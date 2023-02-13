779e080dd9dd2a2acbf08a8952449458740f26c8
prathiksha1990 <prathikshashetty90@gmail.com>
2023-02-13 12:39:24 -0500

    Add files via upload

--- Project 5-Data Pipelines/plugins/operators/data_quality.py
+++ Project 5-Data Pipelines/plugins/operators/data_quality.py
@@ -2,9 +2,9 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-class LoadFactOperator(BaseOperator):
+class DataQualityOperator(BaseOperator):
 
-    ui_color = '#F98866'
+    ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
@@ -13,10 +13,10 @@ class LoadFactOperator(BaseOperator):
                  # conn_id = your-connection-name
                  *args, **kwargs):
 
-        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        self.log.info('DataQualityOperator not implemented yet')
