2df03dd374c6cad2541f37af212a12f6ed40edcf
prathiksha1990 <prathikshashetty90@gmail.com>
2023-04-08 21:12:22 -0400

    Add files via upload

--- Project 5-Data Pipelines/plugins/operators/data_quality.py
+++ Project 5-Data Pipelines/plugins/operators/data_quality.py
@@ -3,20 +3,40 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class DataQualityOperator(BaseOperator):
-
+    """
+    Runs data quality check by passing test SQL query and expected result
+    
+    :param redshift_conn_id: Redshift connection ID
+    :param test_query: SQL query to run on Redshift data warehouse
+    :param expected_result: Expected result to match against result of
+        test_query
+    """
+    
     ui_color = '#89DA59'
-
+    
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 test_query="",
+                 expected_result="",
                  *args, **kwargs):
-
+        
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
-
+        self.redshift_conn_id = redshift_conn_id
+        self.test_query = test_query
+        self.expected_result = expected_result
+    
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        
+        self.log.info("Getting credentials")
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Running test")
+        records = redshift_hook.get_records(self.test_query)
+        if records[0][0] != self.expected_result:
+            raise ValueError(f"""
+                Data quality check failed. \
+                {records[0][0]} does not equal {self.expected_result}
+            """)
+        else:
+            self.log.info("Data quality check passed")
--- Project 5-Data Pipelines/plugins/operators/load_dimension.py
+++ Project 5-Data Pipelines/plugins/operators/load_dimension.py
@@ -8,15 +8,26 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 sql_query="",
+                 table="",
+                 truncate="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_query = sql_query
+        self.table = table
+        self.truncate = truncate
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        """
+          Insert data into dimensional tables from staging events and song data.
+          Using a truncate-insert method to empty target tables prior to load.
+        """
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        if self.truncate:
+            redshift.run(f"TRUNCATE TABLE {self.table}")
+        formatted_sql = self.sql_query.format(self.table)
+        redshift.run(formatted_sql)
+        self.log.info(f"Success: {self.task_id}")
